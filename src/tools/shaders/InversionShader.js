e850e339064672df53d709a032b8d0a92a7068f4
basvanmeurs <basvanmeurs@live.com>
2017-08-18 12:33:30 +0200

    - it works.. but needs some more testing

---
+++ src/tools/shaders/InversionShader.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright Metrological, 2017
+ */
+
+let Shader = require('../../core/Shader');
+
+class InversionShader extends Shader {
+    getFragmentShaderSource() {
+        return InversionShader.fragmentShaderSource
+    }
+}
+
+InversionShader.fragmentShaderSource = `
+    #ifdef GL_ES
+    precision lowp float;
+    #endif
+    varying vec2 vTextureCoord;
+    varying vec4 vColor;
+    uniform sampler2D uSampler;
+    void main(void){
+        vec4 color = texture2D(uSampler, vTextureCoord);
+        color.rgb = 1.0 - color.rgb; 
+        gl_FragColor = color * vColor;
+    }
+`;
+
+module.exports = InversionShader
