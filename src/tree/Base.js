6c519fc9c49754d9ad3c6fd782d6901210481123
basvanmeurs <basvanmeurs@live.com>
2018-03-27 15:35:12 +0200

    cleanup(Base): we don't use es5 mixins any more (we refactored EventEmitter a while ago)

--- src/tree/Base.js
+++ src/tree/Base.js
@@ -3,32 +3,6 @@
  */
 class Base {
 
-    /**
-     * Mixes an ES5 class and the specified superclass.
-     * @param superclass
-     * @param extra
-     *   An ES5 class constructor.
-     */
-    static mixinEs5(superclass, extra) {
-        let proto = extra.prototype;
-
-        let props = Object.getOwnPropertyNames(proto);
-        for(let i = 0; i < props.length; i++) {
-            let key = props[i];
-            let desc = Object.getOwnPropertyDescriptor(proto, key);
-            if (key !== 'constructor' && desc.configurable) {
-                if (superclass.prototype[key]) {
-                    // Mixin may not overwrite prototype methods.
-                    console.warn('Mixin overwrites ' + key);
-                } else {
-                    Object.defineProperty(superclass.prototype, key, desc);
-                }
-            }
-        }
-
-        return superclass;
-    };
-
     static defaultSetter(obj, name, value) {
         obj[name] = value
     }
