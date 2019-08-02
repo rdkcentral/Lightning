a5d3a04cdb87ba491228846f79d7b481a532dea0
mfiess200 <michael_fiess@cable.comcast.com>
2019-08-02 17:39:03 -0400

    code cleanup

--- src/platforms/spark/TextTextureRendererSpark.mjs
+++ src/platforms/spark/TextTextureRendererSpark.mjs
@@ -5,7 +5,7 @@ export default class TextTextureRendererSpark extends TextTextureRenderer {
         let textTextureRender = this;
         let sparkText = sparkscene.create({ t: "text", text:textTextureRender._settings.text, pixelSize:textTextureRender._settings.fontSize*textTextureRender.getPrecision()});
 
-        let drawPromise = new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             sparkText.ready.then( function(obj) {
                 let renderInfo = {};
                 renderInfo.w = sparkText.w;
@@ -17,6 +17,5 @@ export default class TextTextureRendererSpark extends TextTextureRenderer {
                 resolve();
             });
         });
-        return drawPromise;
     };
 }
