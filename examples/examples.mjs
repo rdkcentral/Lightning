61719fdb0e8ec367ef24f09e9ebc651d52d8cbbb
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-08-28 12:29:32 -0400

    Fixed reference

--- examples/examples.mjs
+++ examples/examples.mjs
@@ -1,2 +1,3 @@
 import lng from '../src/lightning.mjs';
 import WaterWaveShader from './water-waves/WaterWaveShader.mjs';
+import WebGLCoreQuadListExample from './WebGLCoreQuadListExample/WebGLCoreQuadListExample.mjs';
