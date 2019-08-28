cd3071d7c9cb8a2c2e34396c66fe5373662c2a3b
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-08-28 12:26:57 -0400

    example mjs

---
+++ examples/examples.mjs
@@ -0,0 +1,2 @@
+import lng from '../../src/lightning.mjs';
+import WaterWaveShader from './water-waves/WaterWaveShader.mjs';
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 const resolve = require('./rollup.plugin.resolver');
 
 export default {
-    input: ['./src/lightning.mjs', './examples/water-waves/WaterWaveShader.mjs'],
+    input: './examples/examples.mjs',
     plugins: [resolve("web")],
     output: {
         file: './dist/lightning-web.js',
