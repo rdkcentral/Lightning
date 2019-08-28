cc197896c2a164ccc09a854d59ee125a92584458
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-08-28 10:07:37 -0400

    Fixed reference

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 const resolve = require('./rollup.plugin.resolver');
 
 export default {
-    input: './src/lightning.mjs', './examples/water-waves/WaterWaveShader.mjs',
+    input: {
+        './src/lightning.mjs', 
+        './examples/water-waves/WaterWaveShader.mjs',
+    },
     plugins: [resolve("web")],
     output: {
         file: './dist/lightning-web.js',
