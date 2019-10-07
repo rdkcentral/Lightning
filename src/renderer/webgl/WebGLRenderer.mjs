54dd53f19b881942ee110f43d4529ee0642f215b
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-10-07 13:26:24 -0400

    cleanup

--- src/renderer/webgl/WebGLRenderer.mjs
+++ src/renderer/webgl/WebGLRenderer.mjs
@@ -1,7 +1,6 @@
 import Utils from "../../tree/Utils.mjs";
 import StageUtils from "../../tree/StageUtils.mjs";
 import WebGLCoreQuadList from "./WebGLCoreQuadList.mjs";
-import SpriteQuadList from "./SpriteQuadList.mjs";
 import WebGLCoreQuadOperation from "./WebGLCoreQuadOperation.mjs";
 import WebGLCoreRenderExecutor from "./WebGLCoreRenderExecutor.mjs";
 import CoreRenderState from "../../tree/core/CoreRenderState.mjs";
