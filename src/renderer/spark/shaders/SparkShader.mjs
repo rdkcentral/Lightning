f75b94132b5ddd47ccb0717c793042718a9d37c4
mfiess200 <michael_fiess@cable.comcast.com>
2020-02-07 18:36:42 -0500

    spark renderer updates

--- src/renderer/spark/shaders/SparkShader.mjs
+++ src/renderer/spark/shaders/SparkShader.mjs
@@ -1,6 +1,42 @@
-import DefaultShader from "../../webgl/shaders/DefaultShader.mjs";
+import WebGLShader from "../../webgl/WebGLShader.mjs";
 
-export default class SparkShader extends DefaultShader {
+export default class SparkShader extends WebGLShader {
+
+    enableAttribs() {
+        // Enables the attribs in the shader program.
+        let gl = this.gl;
+        gl.vertexAttribPointer(this._attrib("aVertexPosition"), 2, gl.FLOAT, false, 20, 0);
+        gl.enableVertexAttribArray(this._attrib("aVertexPosition"));
+
+        if (this._attrib("aTextureCoord") !== -1) {
+            gl.vertexAttribPointer(this._attrib("aTextureCoord"), 2, gl.FLOAT, false, 20, 2 * 4);
+            gl.enableVertexAttribArray(this._attrib("aTextureCoord"));
+        }
+
+        if (this._attrib("aColor") !== -1) {
+            // Some shaders may ignore the color.
+            gl.vertexAttribPointer(this._attrib("aColor"), 4, gl.UNSIGNED_BYTE, true, 20, 4 * 4);
+            gl.enableVertexAttribArray(this._attrib("aColor"));
+        }
+    }
+
+    disableAttribs() {
+        // Disables the attribs in the shader program.
+        let gl = this.gl;
+        gl.disableVertexAttribArray(this._attrib("aVertexPosition"));
+
+        if (this._attrib("aTextureCoord") !== -1) {
+            gl.disableVertexAttribArray(this._attrib("aTextureCoord"));
+        }
+
+        if (this._attrib("aColor") !== -1) {
+            gl.disableVertexAttribArray(this._attrib("aColor"));
+        }
+    }
+
+    setupUniforms(operation) {
+        this._setUniform("projection", this._getProjection(operation), this.gl.uniform2fv, false);
+    }
 
     draw(operation) {
         let gl = this.gl;
@@ -40,3 +76,34 @@ export default class SparkShader extends DefaultShader {
     }
 
 }
+
+SparkShader.vertexShaderSource = `
+    #ifdef GL_ES
+    precision lowp float;
+    #endif
+    attribute vec2 aVertexPosition;
+    attribute vec2 aTextureCoord;
+    attribute vec4 aColor;
+    uniform vec2 projection;
+    varying vec2 vTextureCoord;
+    varying vec4 vColor;
+    void main(void){
+        gl_Position = vec4(aVertexPosition.x * projection.x - 1.0, aVertexPosition.y * -abs(projection.y) + 1.0, 0.0, 1.0);
+        vTextureCoord = aTextureCoord;
+        vColor = aColor;
+        gl_Position.y = -sign(projection.y) * gl_Position.y;
+    }
+`;
+
+SparkShader.fragmentShaderSource = `
+    #ifdef GL_ES
+    precision lowp float;
+    #endif
+    varying vec2 vTextureCoord;
+    varying vec4 vColor;
+    uniform sampler2D uSampler;
+    void main(void){
+        gl_FragColor = texture2D(uSampler, vTextureCoord) * vColor;
+    }
+`;
+
