624b0e564b4ed3bc2cd023e363ebd70d7f80377c
mfiess200 <michael_fiess@cable.comcast.com>
2020-02-11 11:02:10 -0500

    spark updates

--- src/tree/Utils.mjs
+++ src/tree/Utils.mjs
@@ -171,7 +171,7 @@ export default class Utils {
 
 }
 
-Utils.isWeb = (typeof window !== "undefined") && (typeof sparkscene === "undefined");
+Utils.isNode = (typeof window === "undefined");
+Utils.isWeb = (typeof window !== "undefined");
 Utils.isWPE = Utils.isWeb && (navigator.userAgent.indexOf("WPE") !== -1);
-Utils.isSpark = (typeof sparkscene !== "undefined");
-Utils.isNode = (typeof window === "undefined") || Utils.isSpark;
+Utils.isSpark = (typeof window === "undefined") && (typeof sparkscene !== "undefined");
