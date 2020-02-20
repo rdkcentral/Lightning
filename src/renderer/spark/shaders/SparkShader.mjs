1aa4ae9fe5ede5ad0dfd90d64c25a97dccda7e46
Michael Fiess <10201495+mfiess@users.noreply.github.com>
2020-02-20 14:42:57 -0500

    spark shader updates (#113)

--- src/renderer/spark/shaders/SparkShader.mjs
+++ src/renderer/spark/shaders/SparkShader.mjs
@@ -53,7 +53,7 @@ export default class SparkShader extends WebGLShader {
                         let elementPostion = (i > 0) ? (i - 1) : i;
                         const precision = this.ctx.stage.getOption('precision');
                         let vc = operation.getElementCore(elementPostion);
-                        this.ctx.stage.platform.paint(gl, glTexture.options.imageRef, vc._worldContext.px*precision, vc._worldContext.py*precision, vc._colorUl);
+                        this.ctx.stage.platform.paint(gl, glTexture.options.imageRef, vc._worldContext.px*precision, vc._worldContext.py*precision, vc._colorUl, vc);
                     } else {
                         gl.bindTexture(gl.TEXTURE_2D, glTexture);
                         gl.drawElements(gl.TRIANGLES, 6 * (i - pos), gl.UNSIGNED_SHORT, (pos + operation.index) * 6 * 2);
@@ -66,7 +66,7 @@ export default class SparkShader extends WebGLShader {
                 if (glTexture.options && glTexture.options.imageRef) {
                     const precision = this.ctx.stage.getOption('precision');
                     let vc = operation.getElementCore(pos);
-                    this.ctx.stage.platform.paint(gl, glTexture.options.imageRef, vc._worldContext.px*precision, vc._worldContext.py*precision, vc._colorUl);
+                    this.ctx.stage.platform.paint(gl, glTexture.options.imageRef, vc._worldContext.px*precision, vc._worldContext.py*precision, vc._colorUl, vc);
                 } else {
                     gl.bindTexture(gl.TEXTURE_2D, glTexture);
                     gl.drawElements(gl.TRIANGLES, 6 * (length - pos), gl.UNSIGNED_SHORT, (pos + operation.index) * 6 * 2);
