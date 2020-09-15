5c8771112b74230e2eb8c1f0df66791f91720397
Grzegorz Zachar <g.zachar@metrological.com>
2020-09-15 11:54:04 +0200

    POC

--- src/application/Component.mjs
+++ src/application/Component.mjs
@@ -76,6 +76,36 @@ export default class Component extends Element {
         }
     }
 
+    static bindProperty(name, func = null) {
+        return {__propertyBinding: true, __name: name, __func: func};
+    }
+
+    __bindProperty(propObj, targetObj, targetProp) {
+        console.log('__bindProperty', targetObj, targetProp, propObj);
+
+        // 1. find binding position: find object and property name to be bound
+        const obj = targetObj;
+        const prop = targetProp;
+        const propName = propObj.__name;
+        const func = propObj.__func ? propObj.__func : (context) => context[propName];
+
+        // 2. create setter for given object
+        if (!this.hasOwnProperty(propName)) {
+            this[`__prop_bindings_${propName}`] = [{__obj: obj, __prop: prop, __func: func}];
+            Object.defineProperty(this, propName, {
+                set: (value) => {
+                    this[`__prop_${propName}`] = value;
+                    for (const { __obj, __prop, __func} of this[`__prop_bindings_${propName}`]) {
+                        __obj[__prop] = __func(this);
+                    }
+                },
+                get: () => this[`__prop_${propName}`]
+            });
+        } else {
+            this[`__prop_bindings_${propName}`].push({__obj: obj, __prop: prop, __func: func});
+        }
+    }
+
     /**
      * Returns a high-performance template patcher.
      */
@@ -153,6 +183,10 @@ export default class Component extends Element {
                         loc.push(`${propKey} = ${cursor}.texture`);
                         this.parseTemplatePropRec(value, context, propKey);
                     }
+                } else if (Utils.isObjectLiteral(value) && value.__propertyBinding === true) {
+                    // console.log('PROP BINDING DETECTED', key, value)
+                    store.push(value)
+                    loc.push(`element.__bindProperty(store[${store.length - 1}], ${cursor}, "${key}")`);
                 } else {
                     // Property;
                     if (Utils.isNumber(value)) {
@@ -184,6 +218,10 @@ export default class Component extends Element {
                     loc.push(`${cursor}["${key}"] = ${value}`);
                 } else if (Utils.isBoolean(value)) {
                     loc.push(`${cursor}["${key}"] = ${value ? "true" : "false"}`);
+                } else if (Utils.isObject(value) && value.__propertyBinding === true) {
+                    // console.log('PROP BINDING DETECTED', key, value)
+                    store.push(value)
+                    loc.push(`element.__bindProperty(store[${store.length - 1}], ${cursor}, "${key}")`);
                 } else if (Utils.isObject(value) || Array.isArray(value)) {
                     // Dynamic assignment.
                     // Because literal objects may contain dynamics, we store the full object.
