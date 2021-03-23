a5a28374b435115809a54c3ac561af54549e1e54
Grzegorz Zachar <g.zachar@metrological.com>
2021-03-23 11:12:40 +0100

    Fixed texture conversion from canvas

--- src/tree/Element.mjs
+++ src/tree/Element.mjs
@@ -2057,7 +2057,8 @@ export default class Element {
 
     toJSON() {
         const ref = [`${this.constructor.name}`];
-        const tree = {[ref]: {}};
+        const tree = {};
+        tree[ref] = {};
 
         if (this.hasChildren()) {
             Element.collectChildren(tree[ref], this.__childList);
