ae36afadb19cfa4f7e3faa3331ba77d4ee9a8470
Wouter van Boesschoten <wouter@wouterlucas.com>
2023-06-28 15:28:18 +0200

    Fix alpha channel detection when using in-process image handling.
    This fix solves an issue when the image worker is not being used, so Lightning processes images on the main thread. There is a simple check for `.png` to toggle whether or not the image has an alpha channel. This however gives an issue when a `.PNG` is used. Introduced a simple lowerCase cast to be able to work with both `.png` and `.PNG`.
    
    Additionally added some (quick and dirty) vitest cases to validate the above changes.
    Resolves: https://github.com/rdkcentral/Lightning/issues/493

--- src/platforms/browser/WebPlatform.mjs
+++ src/platforms/browser/WebPlatform.mjs
@@ -267,7 +267,7 @@ export default class WebPlatform {
 
     loadSrcTexture({ src, hasAlpha }, cb) {
         let cancelCb = undefined;
-        let isPng = (src.indexOf(".png") >= 0) || src.substr(0, 21) == 'data:image/png;base64';
+        let isPng = (src.toLowerCase().indexOf(".png") >= 0) || src.substr(0, 21) == 'data:image/png;base64';
         let isKtx = src.indexOf('.ktx') >= 0;
         let isPvr = src.indexOf('.pvr') >= 0;
         if (isKtx || isPvr) {
