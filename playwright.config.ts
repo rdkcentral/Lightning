bd5b4687e427d4533c3996c589cee74ac5b0758f
Philippe Elsass <philippe.elsass@gmail.com>
2025-02-28 12:31:32 +0100

    Increase test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Maximum run time of one test */
+  timeout: 120_000,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
