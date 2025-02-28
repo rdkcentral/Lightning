9cd78b4001cbde66f43cd8fe6f4d9cc46cda4d26
Philippe Elsass <philippe.elsass@gmail.com>
2025-02-28 15:20:40 +0100

    Disable webkit test

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,10 +40,10 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    {
+    /*{
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
-    },
+    },*/
   ],
 
   /* Run your local dev server before starting the tests */
