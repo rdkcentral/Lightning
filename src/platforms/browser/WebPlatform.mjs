0489e059405e2ad8f9bf5796659730ef1039330f
Ugur Aslan <ugur@uguraslan.net>
2025-03-11 20:15:29 +0100

    updated url check

--- src/platforms/browser/WebPlatform.mjs
+++ src/platforms/browser/WebPlatform.mjs
@@ -290,10 +290,16 @@ export default class WebPlatform {
             }
         } else if (this._imageWorker) {
             // WPE-specific image parser.
-            // image urls can be like "//example.com/image.png"
-            if (typeof src !== 'string' || (src.indexOf('://') < 0 && !src.startsWith('//'))){
+            if (typeof src !== 'string') {
                 return cb("Invalid image URL");
             }
+            
+            // URL can start with http://, https://, and //
+            const separatorPos = src.indexOf('//');
+            if (separatorPos !== 0 && separatorPos !== 5 && separatorPos !== 6) {
+                return cb("Invalid image URL");
+            }
+
             const image = this._imageWorker.create(src);
             image.onError = function (err) {
                 return cb("Image load error");
